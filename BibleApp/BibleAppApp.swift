5b3bd9878b31b3a58c24798d6194882f2de27758
Marvin John <Marvinc.john.03@gmail.com>
2024-11-17 20:52:24 +0100

    Initial Commit

---
+++ BibleApp/BibleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleAppApp.swift
+//  BibleApp
+//
+//  Created by Marvin John on 17.11.24.
+//
+
+import SwiftUI
+
+@main
+struct BibleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BibleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BibleApp
+//
+//  Created by Marvin John on 17.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
